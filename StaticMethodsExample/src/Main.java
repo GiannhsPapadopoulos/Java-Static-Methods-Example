f6c14c81fc0df38f5ef445e5becb6e2307f019e2
JP <papadopoulos.giannhs.soft@gmail.com>
2026-03-05 08:39:44 +0200

    Add files via upload

---
+++ StaticMethodsExample/src/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+
+    public static void printHello() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        Calculator.printSum(5, 10);
+        printHello(); // shorter from of Main.printHello();
+
+    }
+}
